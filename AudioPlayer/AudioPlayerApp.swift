356e9fc2bd059746caa251dbb5550bc608a42df5
andrey <avpetrov@derzhava.online>
2024-07-24 11:05:33 +0300

    initial commit

---
+++ AudioPlayer/AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerApp.swift
+//  AudioPlayer
+//
+//  Created by Андрей on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
